abe4387c502d3e3bfe8147bfe0731315c18edf4a
aeris.ahir <aeris.ahir@1R-75>
2022-02-15 17:46:47 +0530

    Add project files.

---
+++ nullls/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nullls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "mobile tearm";
+            string s2 = "";
+            string s3 = null;
+
+
+            bool b1 = string.IsNullOrEmpty(s1);
+            bool b2 = string.IsNullOrEmpty(s2);
+            bool b3 = string.IsNullOrEmpty(s3);
+            Console.WriteLine(b1);
+            Console.ReadLine();
+            Console.WriteLine(b2);
+            Console.ReadLine();
+            Console.WriteLine(b3);
+            Console.ReadLine();
+        }   }
+}
